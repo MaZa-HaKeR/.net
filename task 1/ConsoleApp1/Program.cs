cb30051590da85c30aef993698e88ebfc261db6b
MaZa-HaKeR <mazahaker17@gmail.com>
2020-02-26 15:48:29 +0200

    Tasks
    Task 1,2

---
+++ task 1/ConsoleApp1/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            foreach(var item in Environment.GetLogicalDrives())
+            {
+                Console.WriteLine("Drive: {0}",item);
+            }
+            Console.WriteLine(".NET Version {0}", Environment.Version);
+            Console.WriteLine("OS {0}", Environment.OSVersion);
+
+            Console.WriteLine("Please enter your name: ");
+            string username = Console.ReadLine();
+            Console.WriteLine("Please enter your Firstname: ");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("Please enter your age: ");
+            string age = Console.ReadLine();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            ConsoleColor prevcolor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Hello {0} {1}! You are {2} years old.",username,firstname,age);
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = prevcolor;
+            Console.WriteLine("{0}, {1}", Console.CapsLock, Console.NumberLock);
+            Console.ReadLine();
+            */
+            //1
+            string first = Console.ReadLine();
+            char[] newstr = first.ToCharArray();
+            Array.Reverse(newstr);
+            string first1= new string (newstr);
+            Console.WriteLine(first1);
+
+
+            //2
+            string second = Console.ReadLine();
+            int numb = Convert.ToInt32(Console.ReadLine());
+            char[] experim = second.ToCharArray();
+            for (int i = numb-1; i <= second.Length-numb+2 ; i += numb)
+            {
+                experim[i]= char.ToUpper(experim[i]);
+            }
+            string hol = new string(experim);
+            Console.WriteLine(hol);
+
+            //3
+            string second1 = Console.ReadLine();
+            int numb1 = Convert.ToInt32(Console.ReadLine());
+            char[] experim1 = second1.ToCharArray();
+            experim1[numb1 - 1]=char.ToUpper(experim1[numb1-1]);
+            Console.WriteLine(experim1);
+
+            //4
+            string st = Console.ReadLine();
+            char[] st1 = st.ToCharArray();
+            bool flag = true;
+            for (int i = 0; i < st.Length ; i++)
+            {
+                if (char.IsLetter(st1[i]))
+                {
+                    flag = false;
+                    Console.WriteLine("String contains letter(s)");
+                    break;
+                }
+            }
+            if (flag == true)
+                Console.WriteLine("String consist of Numbers");
+
+            //5
+            string st5 = Console.ReadLine();
+            char[] st5ch = st5.ToCharArray();
+            bool flag1 = true;
+            //1
+            for (int i = 0, j = st5.Length - 1; i < j; i++, j--)
+            { if (!st5ch[i].Equals(st5ch[j]))
+                    {
+                    flag1 = false; 
+                    Console.WriteLine("String is not polindrom");
+                    break;
+                } }
+            Array.Reverse(st5ch);
+            string st52 = new string(st5ch);
+            if (st52 != st5)
+            {
+                flag1 = false;
+                Console.WriteLine("String is not polindrom");
+            }
+                if (flag1 == true) Console.WriteLine("String is polindrom");
+
+            //6
+            int num1 = Convert.ToInt32(Console.ReadLine()),
+                num2=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num1 * num2);
+
+            //7
+            string har = Console.ReadLine();
+            for(int i=0;i<har.Length;i++)
+            {
+                char[] har1 = har.ToCharArray();
+                if (char.IsLetter(har1[i]))
+                {
+                    har=har.Remove(i, 1);
+                    i--;
+                }
+            }
+            Console.WriteLine(har);
+            //8
+            string har2 = Console.ReadLine();
+            for (int i = 0; i < har2.Length; i++)
+            {
+                char[] har12 = har2.ToCharArray();
+                if (char.IsWhiteSpace(har12[i]))
+                {
+                    har2 = har2.Remove(i, 1);
+                    i--;
+                }
+            }
+            Console.WriteLine(har2);
+            Console.ReadLine();
+        }
+    }
+}
+//msdn methods runs
+//console environment
---
+++ task 2/task 2/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+
+namespace task_2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int choose =0;
+            Console.WriteLine("Input number of task(1-6):");
+            choose = Convert.ToInt32(Console.ReadLine());
+            if (choose == 1)
+            {
+                First();
+            }
+            if (choose == 2)
+            {
+                Console.WriteLine("Input array dimensions: ");
+                int value = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Input type data:\nint-1\nlong-2\nbyte-3");
+                int type=Convert.ToInt32(Console.ReadLine());
+                switch (type)
+                {
+                    case 1:
+                        Second(value);
+                        break;
+                    case 2:
+                        Second(Convert.ToInt64(value));
+                        break;
+                    case 3:
+                        Second(Convert.ToByte(value));
+                        break;
+                }
+            }
+            if (choose == 3)
+            {
+                int numb;
+                Console.WriteLine("Input numb(1-500):");
+                numb = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("This numb is in massive {0} times", Thirth(numb)); ;
+            }
+            if (choose == 4)
+            {
+                Console.WriteLine("Input Date time as '{0}'",DateTime.Today);
+                DateTime first = Convert.ToDateTime(Console.ReadLine());
+                DateTime second = Convert.ToDateTime(Console.ReadLine());
+                Forth(first,second);
+            }
+            if (choose == 5)
+            {
+                Console.WriteLine("Input Date time as '{0}'", DateTime.Today);
+                DateTime date = Convert.ToDateTime(Console.ReadLine());
+                Console.WriteLine("*Input* Plus or minus n days?(true - plus, false - minus)");
+                bool flag=Convert.ToBoolean(Console.ReadLine());
+                Fifth(date,flag);
+            }
+            if (choose == 6)
+            {
+                Console.WriteLine("Input string: ");
+                string str = Console.ReadLine();
+                Console.WriteLine("Input searching letter: ");
+                char letter = Console.ReadLine().ToCharArray()[0];
+                Console.WriteLine(Sixth(str, letter));
+            }
+            Console.ReadLine();
+        }
+        static void First()
+        {
+            Console.WriteLine("Ура сегодня {0}", (days)(int)DateTime.Now.DayOfWeek);
+        }
+        public enum days
+        {
+            Понедельник=1,
+            Вторник,
+            Среда,
+            Четверг,
+            Пятница,
+            Суббота,
+            Воскресенье
+        }
+        static void Second(int val)
+        {
+            Random rand = new Random();
+            int[] arr = new int[val];
+            for (int i = 0; i < val; i++)
+            {
+                arr[i] = rand.Next(500);
+            }
+            int max = arr[0], min = arr[0], av = 0;
+            foreach (int elem in arr)
+            {
+                if (elem > max) max = elem;
+                if (elem < min) min = elem;
+                av += elem;
+            }
+            Console.WriteLine("minValue = {0}\n maxValue = {1}\n Average = {2}\n", min, max, av / val);
+        }
+        static void Second(long val)
+        {
+            Random rand = new Random();
+            long[] arr = new long[val];
+            for (int i = 0; i < val; i++)
+            {
+                arr[i] = rand.Next(500);
+            }
+            long max = arr[0], min = arr[0], av = 0;
+            foreach (int elem in arr)
+            {
+                if (elem > max) max = elem;
+                if (elem < min) min = elem;
+                av += elem;
+            }
+            Console.WriteLine("minValue = {0}\n maxValue = {1}\n Average = {2}\n", min, max, av / val);
+        }
+
+        static void Second(byte val)
+        {
+            Random rand = new Random();
+            int[] arr = new int[val];
+            for (int i = 0; i < val; i++)
+            {
+                arr[i] = rand.Next(500);
+            }
+            int max = arr[0], min = arr[0], av = 0;
+            foreach (int elem in arr)
+            {
+                if (elem > max) max = elem;
+                if (elem < min) min = elem;
+                av += elem;
+            }
+            Console.WriteLine("minValue = {0}\n maxValue = {1}\n Average = {2}\n", min, max, av / val);
+        }
+        static int Thirth(int numb)
+        {
+            Random rand = new Random();
+            int[] arr = new int[1000];
+            int search = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                arr[i] = rand.Next(500);
+                if (arr[i].Equals(numb)) search++;
+            }
+
+            return search;
+        }
+
+        static void Forth(DateTime first, DateTime second)
+        {
+            Console.WriteLine(first);
+            Console.WriteLine(second);
+            var timespans = (first - second).TotalSeconds;
+            var timespanm = (first - second).TotalMinutes;
+            var timespand = (first - second).TotalDays;
+            Console.WriteLine("Seconds = {0}, minutes = {1}, days = {2}",timespans,timespanm,timespand);
+        }
+
+        static void Fifth(DateTime date, bool flag)
+        {
+            Random rand = new Random();
+            int n = rand.Next(72, 1000);
+            if (flag)
+            {
+                date = date.AddDays(n);
+                Console.WriteLine("Date: {0}, {1}", date, date.DayOfWeek);
+            }
+            else
+            {
+                date = date.AddDays(-n);
+                Console.WriteLine("Date: {0}, {1}", date, date.DayOfWeek);
+            }
+        }
+        static int Sixth(string str,char search)
+        {
+            int sear = 0;
+            char[] strch = str.ToCharArray();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (strch[i].Equals(search)) sear++;
+            }   
+            return sear;
+        }
+    }
+}
